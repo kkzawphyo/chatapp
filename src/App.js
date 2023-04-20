3c4d87de32e0241071d7af42594fa21c6d228a0d
zawphyo <pmzawphyo1@gmail.com>
2023-04-20 09:36:50 +0900

    chatapp

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '305bce74-f094-484c-a323-e0b620b2a518';
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+export default App;
+
